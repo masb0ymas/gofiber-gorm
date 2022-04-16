45d1e0a3132f3632c8a3109cc7c7b11a3d8fd22c
N. Fajri <fajriajjha@gmail.com>
2022-04-16 13:16:06 +0700

    fix: model response

--- src/models/response/response.go
+++ src/models/response/response.go
@@ -1,7 +1,51 @@
 package response
 
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 type Response struct {
 	Code    int         `json:"code,omitempty"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
+}
+
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
+type ErrorBody struct {
+	Code    int             `json:"code,omitempty"`
+	Message string          `json:"message,omitempty"`
+	Error   []*ErrorMessage `json:"errors"`
+}
+
+func mapToErrorOutput(e *fiber.Error) *ErrorMessage {
+	return &ErrorMessage{
+		Message: e.Message,
+	}
+}
+
+func HttpResponse(code int, message string, data interface{}) *Response {
+	return &Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func HttpErrorResponse(errorObj []*fiber.Error) *ErrorBody {
+	var errorSlice []*ErrorMessage
+
+	for i := 0; i < len(errorObj); i++ {
+		errorSlice = append(errorSlice, mapToErrorOutput(errorObj[i]))
+	}
+
+	return &ErrorBody{
+		Code:    http.StatusBadRequest,
+		Message: "Unprocessable Entity",
+		Error:   errorSlice,
+	}
 }

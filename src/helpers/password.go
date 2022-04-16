75a851f194aea2f272e6120bb5f947c1b25b155d
N. Fajri <fajriajjha@gmail.com>
2022-04-16 13:15:52 +0700

    feat: add helper hash password

---
+++ src/helpers/password.go
@@ -0,0 +1,17 @@
+package helpers
+
+import "golang.org/x/crypto/bcrypt"
+
+// hash password before store DB
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(bytes), err
+}
+
+// compare password
+func ComparePassword(hash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
+}

10fe2505b555c7e418684e1eaeb3b5fad358456f
N. Fajri <fajriajjha@gmail.com>
2022-04-16 13:16:24 +0700

    feat: add role entity

--- src/models/entity/role.entity.go
+++ src/models/entity/role.entity.go
@@ -1,5 +1,17 @@
 package entity
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Role struct {
-	Name string `json:name,validate:required`
+	gorm.Model
+	ID        uuid.UUID      `json:"id" gorm:"type:uuid"`
+	Name      string         `json:"name" validate:"required"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }

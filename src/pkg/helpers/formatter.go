f22aa2f8a5ba841a259a8a618e3934cf5c90bbe5
masb0ymas <n.fajri@outlook.com>
2022-10-30 12:28:57 +0700

    feat: add helper formatter

---
+++ src/pkg/helpers/formatter.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// NullString is a wrapper around sql.NullString
+type NullString sql.NullString
+
+// MarshalJSON method is called by json.Marshal,
+// whenever it is of type NullString
+func (x *NullString) MarshalJSON() ([]byte, error) {
+	if !x.Valid {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(x.String)
+}
+
+func Pretty(data interface{}) {
+	b, err := json.MarshalIndent(data, "", " ")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
+}

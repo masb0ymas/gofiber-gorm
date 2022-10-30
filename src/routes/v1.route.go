5be9bae7039135eea417f64101b52c79e9b17577
masb0ymas <n.fajri@outlook.com>
2022-10-30 16:18:40 +0700

    fix: add auth route & protect group role

--- src/routes/v1.route.go
+++ src/routes/v1.route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gofiber-gorm/src/app/controllers"
+	"gofiber-gorm/src/app/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,8 +15,13 @@ func RouteV1(app *fiber.App) {
 		return c.Next()
 	})
 
+	// Auth Route
+	authRoute := v1.Group("/auth")
+	authRoute.Post("/sign-up", controllers.Register)
+	authRoute.Post("/sign-in", controllers.Login)
+
 	// Role Route
-	roleRoute := v1.Group("/role")
+	roleRoute := v1.Group("/role", middlewares.AuthMiddleware())
 	roleRoute.Get("/", controllers.FindAllRole)
 	roleRoute.Get("/:id", controllers.FindRoleById)
 	roleRoute.Post("/", controllers.CreateRole)
@@ -23,4 +29,8 @@ func RouteV1(app *fiber.App) {
 	roleRoute.Put("/restore/:id", controllers.RestoreRoleById)
 	roleRoute.Delete("/soft-delete/:id", controllers.SoftDeleteRoleById)
 	roleRoute.Delete("/force-delete/:id", controllers.ForceDeleteRoleById)
+
+	// User Route
+	userRoute := v1.Group("/user")
+	userRoute.Get("/", middlewares.AuthMiddleware(), controllers.FindAllUser)
 }

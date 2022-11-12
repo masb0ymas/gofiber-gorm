64dd257e33bdb96534b59d43bd0f5b3ba114bb7a
masb0ymas <n.fajri@outlook.com>
2022-11-12 18:40:55 +0700

    feat: add role constant

---
+++ src/pkg/constants/role.constant.go
@@ -0,0 +1,5 @@
+package constants
+
+const ROLE_SUPER_ADMIN = "ded1d39a-1dbe-4f54-923e-e9643aed76e5"
+const ROLE_ADMIN = "1a27cca3-bfdd-4a0c-95ab-8de60f55c1cc"
+const ROLE_USER = "dd622ac9-b5b8-4b78-a913-4e2b5fc08e3c"

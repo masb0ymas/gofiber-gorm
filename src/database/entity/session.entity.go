d37534925981ff014a297d93d853d42d074f2174
masb0ymas <n.fajri@outlook.com>
2022-11-13 22:20:14 +0700

    fix: session entity

--- src/database/entity/session.entity.go
+++ src/database/entity/session.entity.go
@@ -1,14 +1,11 @@
 package entity
 
-import "gopkg.in/guregu/null.v4"
-
 // Session struct to describe role object.
 type Session struct {
-	Base                  // Base Entity
-	Token     string      `json:"token" gorm:"not null" validate:"required"`
-	IpAddress string      `json:"ip_address" gorm:"not null" validate:"required"`
-	Device    null.String `json:"device"`
-	Platform  null.String `json:"platform"`
-	UserId    string      `json:"user_id" gorm:"type:uuid; not null" validate:"required"`
-	User      User        `json:"User"`
+	Base             // Base Entity
+	Token     string `json:"token" gorm:"not null" validate:"required"`
+	IpAddress string `json:"ip_address" gorm:"not null" validate:"required"`
+	UserAgent string `json:"user_agent" gorm:"type:text; not null"`
+	UserId    string `json:"user_id" gorm:"type:uuid; not null" validate:"required"`
+	User      User   `json:"User"`
 }
--- src/database/schema/session.schema.go
+++ src/database/schema/session.schema.go
@@ -1,11 +1,8 @@
 package schema
 
-import "gopkg.in/guregu/null.v4"
-
 type SessionSchema struct {
-	Token     string      `json:"token" validate:"required"`
-	IpAddress string      `json:"ip_address" validate:"required"`
-	Device    null.String `json:"device"`
-	Platform  null.String `json:"platform"`
-	UserId    string      `json:"user_id" validate:"required"`
+	Token     string `json:"token" validate:"required"`
+	IpAddress string `json:"ip_address" validate:"required"`
+	UserAgent string `json:"user_agent" validate:"required"`
+	UserId    string `json:"user_id" validate:"required"`
 }

9d2391329cbc5b615c4409fd91bc9451a099dfa4
masb0ymas <n.fajri@outlook.com>
2023-05-18 22:00:22 +0700

    fix: improve helper formatter

--- src/pkg/helpers/formatter.go
+++ src/pkg/helpers/formatter.go
@@ -1,25 +1,52 @@
 package helpers
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
+	"unicode"
 
+	"github.com/fatih/color"
 	"github.com/google/uuid"
 )
 
-// NullString is a wrapper around sql.NullString
-type NullString sql.NullString
+type Options struct {
+	Label string
+}
 
-// MarshalJSON method is called by json.Marshal,
-// whenever it is of type NullString
-func (x *NullString) MarshalJSON() ([]byte, error) {
-	if !x.Valid {
-		return []byte("null"), nil
+// Print Log
+func PrintLog(title string, message string, options ...Options) string {
+	var labelType string
+
+	cyan := color.New(color.FgCyan).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	blue := color.New(color.FgBlue).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+
+	logName := green("[server]:")
+
+	if options == nil {
+		labelType = blue(title)
 	}
 
-	return json.Marshal(x.String)
+	// check object struct
+	for _, opt := range options {
+		if opt.Label == "success" {
+			labelType = blue(title)
+		} else if opt.Label == "warning" {
+			labelType = yellow(title)
+		} else if opt.Label == "error" {
+			labelType = red(title)
+		} else {
+			labelType = blue(title)
+		}
+	}
+
+	newMessage := cyan(message)
+	result := fmt.Sprintf("%s %s %s", logName, labelType, newMessage)
+
+	return result
 }
 
 // Pretty JSON
@@ -40,4 +67,30 @@ func ParseUUID(value any) uuid.UUID {
 	newUID := uuid.Must(uuid.ParseBytes(byteUID))
 
 	return newUID
+}
+
+// check uuid is valid
+func IsValidUUID(u string) bool {
+	_, err := uuid.Parse(u)
+	return err == nil
+}
+
+// check value is digit
+func IsDigit(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// check value is number
+func IsNumber(s string) bool {
+	for _, c := range s {
+		if !unicode.IsNumber(c) {
+			return false
+		}
+	}
+	return true
 }

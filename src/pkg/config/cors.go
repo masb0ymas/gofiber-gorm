fd2cd71683bf0a79d9bdb40b9234bc96ae1c194f
masb0ymas <n.fajri@outlook.com>
2023-05-18 21:59:27 +0700

    fix: cors

--- src/pkg/config/cors.go
+++ src/pkg/config/cors.go
@@ -1,10 +1,23 @@
 package config
 
-import "github.com/gofiber/fiber/v2/middleware/cors"
+import (
+	"fmt"
+	"gofiber-gorm/src/pkg/constants"
+	"gofiber-gorm/src/pkg/helpers"
+	"strings"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func Cors() cors.Config {
+	// list allowed origins
+	allowedOrigins := strings.Join(constants.AllowedOrigins(), ", ")
+
+	logMessage := helpers.PrintLog("Cors", "Allowed Origins ( "+allowedOrigins+" )")
+	fmt.Println(logMessage)
+
 	config := cors.Config{
-		AllowOrigins: "http://localhost:3000, http://localhost:3333",
+		AllowOrigins: allowedOrigins,
 		// AllowMethods:  "GET, POST, HEAD, PUT, DELETE, PATCH",
 		// AllowHeaders:  "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token",
 		// ExposeHeaders: "Content-Length",

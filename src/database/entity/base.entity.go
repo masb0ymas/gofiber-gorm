a510affc0ee156caffa47db32e59bdea4edb8f91
masb0ymas <n.fajri@outlook.com>
2023-05-18 21:57:56 +0700

    fix: base entity

--- src/database/entity/base.entity.go
+++ src/database/entity/base.entity.go
@@ -8,7 +8,6 @@ import (
 )
 
 type Base struct {
-	// gorm.Model
 	ID        uuid.UUID      `json:"id" gorm:"unique; type:uuid; column:id; default:uuid_generate_v4(); not_null"`
 	CreatedAt time.Time      `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time      `json:"updated_at" gorm:"default:CURRENT_TIMESTAMP"`
